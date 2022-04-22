92eb3ec3de1aba21069cff7fffca1a2615b4f433
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2022-04-22 16:40:06 +0100

    added list checks

--- Tutorial/ifstatements.py
+++ Tutorial/ifstatements.py
@@ -1,17 +1,29 @@
 # if statements work like normal. you give a condition and you give a command
 
-name = input("What is your name?")
-name = name.title()
+# name = input("What is your name?")
+# name = name.title()
 # here, I got the name from the user, and capitalized the first letter with the .title function
 
-age = input("How old are you?")
+# age = input("How old are you?")
 
 # you can also use the and statement in place of or. both statements have to be
-if name == "Chxnedu" and str(age) > str(15):
-    print("You are welcome, Master " + name)
+# if name == "Chxnedu" and str(age) > str(15):
+#    print("You are welcome, Master " + name)
 
 # the elif statement stands for else if. its used to check another pair of statements
-elif name == "Chxnedu" and str(age) == str(14):
-    print("Welcome " + name + ", You are of age")
+# elif name == "Chxnedu" and str(age) == str(14):
+#    print("Welcome " + name + ", You are of age")
+# else:
+#   print("Getat! You no be my Master and you're underage")
+
+# you can use if statements to check if a value is in a list
+available_games = ["tlou2","god of war", "bloodborne", "spiderman", "control"]
+
+game = input("Which game would you like to get?")
+
+if game in available_games:
+    print("it's available!")
 else:
-    print("Getat! You no be my Master and you're underage")
+    print("sorry, we dont have it")
+
+
