cfc512ff1b586ba17c6ad9c04c38adc0f73033d3
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2022-04-23 18:20:04 +0100

    added dictionaries tutorial

--- Tutorial/dictionaries.py
+++ Tutorial/dictionaries.py
@@ -22,3 +22,7 @@ infectedHealth = {
 
 bloaterHealthBar = infectedHealth['Bloater']
 print('the bloater has a huge health bar, its maximum health is ' + bloaterHealthBar)
+
+# to add a new key-value pair to the dictionary, we use
+infectedHealth['dogs'] = '10'
+print(infectedHealth)
