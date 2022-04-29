90e11508d87718db6ecd4b4447c68a714e520ed9
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2022-04-29 21:12:15 +0100

    added another mini movie ticket project

---
+++ Projects/movieticketsales.py
@@ -0,0 +1,38 @@
+# a small project to test what I have learned so far
+# A movie theater charges different ticket prices depending on a person’s age. If a person is under the age of 3,
+# the ticket is free; if they are between 3 and 12, the ticket is $10; and if they are over age 12,
+# the ticket is $15. Write a loop in which you ask users their age, and then tell them the cost of their movie
+# ticket.
+
+ticket_prices = ["free", "$10", "$15"]
+# age = input("How old are you? ")
+# first solution using if statements and lists
+# if int(age) < 3:
+#    print("Your ticket is " + ticket_prices[0])
+# elif 3 <= int(age) <= 12:
+#    print("Your ticket is " + ticket_prices[1])
+# elif int(age) > 12:
+#    print("Your ticket is " + ticket_prices[2])
+
+# second solution using while loops
+active = True
+
+while active:
+    age = input("How old are you? ")
+
+    if int(age) < 3:
+        print("Your ticket is " + ticket_prices[0])
+    elif 3 <= int(age) <= 12:
+        print("Your ticket is " + ticket_prices[1])
+    elif int(age) > 12:
+        print("Your ticket is " + ticket_prices[2])
+    else:
+        active = False
+
+
+
+
+
+
+
+
