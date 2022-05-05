4f6e908d413dbb1796898238f3e7bb1167759fea
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2022-05-05 18:38:52 +0100

    added lessons on classes and objects

---
+++ Tutorial/Phone.py
@@ -0,0 +1,10 @@
+# classes are used to define your own data type. the following code below is going to show you how to
+# create classes
+
+class Phone:
+
+    def __init__(self, brand, ram, rom, refresh_rate):
+        self.brand = brand
+        self.ram = ram
+        self.rom = rom
+        self.refresh_rate = refresh_rate
---
+++ Tutorial/classes&objects.py
@@ -0,0 +1,6 @@
+from Phone import Phone
+
+galaxys10 = Phone("Samsung", "8gb", "128gb", "60hz")
+
+print(galaxys10.rom)
+
