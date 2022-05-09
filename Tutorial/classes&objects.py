cf9de83ace40b6416f9340f334ada56ed839765f
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2022-05-09 19:13:06 +0100

    added a small lesson on inheritance

--- Tutorial/classes&objects.py
+++ Tutorial/classes&objects.py
@@ -1,9 +1,11 @@
 from Phone import Phone
+from inheriatance import expensive
 
 galaxys10 = Phone("Samsung", "8gb", "128gb", 60)
 iphone11 = Phone("Apple", "4gb", "64gb", 60)
 pixel4 = Phone("Google", "6gb", "128gb", 90)
 
+oneplus10pro =
 print(pixel4.has_high_refreshrate())
 
 # here, we added a function to check if a particular phone has a high refresh rate
---
+++ Tutorial/inheriatance.py
@@ -0,0 +1,7 @@
+# you can make a new class and it can inherit the functionality of an existing class.
+from Phone import Phone
+
+class expensive(Phone):
+
+    def flagships(self):
+        print('a flagship is the top of the line phone from a company')
